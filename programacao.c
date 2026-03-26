57096df7fd543251a7c92e3149b55c131cc85e49
Nildo Dev/Web <oficial.nildo@gmail.com>
2026-03-26 11:48:05 -0300

    apaguei comentario teste

--- programacao.c
+++ programacao.c
@@ -1,5 +1,5 @@
 #include <stdio.h> 
 int main () {
-    printf("ola, mundo!\n"); // ESTE PROGRAMA CALCULA A NOTA DO ALUNO
-    return 0; //MEU PRIMEIRO PROJETO TESTE
+    printf("ola, mundo!\n"); 
+    return 0; 
 } 
